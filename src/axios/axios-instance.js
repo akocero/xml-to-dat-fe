43e09bea908412e348a6f768158e1d4a187cb6ed
Eugene Badato <badatoeugenepaulm@gmail.com>
2021-08-13 21:56:43 +0800

    feat: aws

--- src/axios/axios-instance.js
+++ src/axios/axios-instance.js
@@ -3,8 +3,8 @@ import axios from 'axios'
 const instance = axios.create({
     //baseURL: 'https://payroll-ent-cloud.herokuapp.com/api/', // heroku
     //baseURL: 'http://159.138.137.115:5000/', // server
-    baseURL: 'http://pecbe-env.eba-jrab52bp.us-east-2.elasticbeanstalk.com/api/',
-    //baseURL: 'http://127.0.0.1:8000/api/',
+    // baseURL: 'http://pecbe-env.eba-jrab52bp.us-east-2.elasticbeanstalk.com/api/',
+    baseURL: 'http://127.0.0.1:8000/api/',
 });
 
 export default instance
