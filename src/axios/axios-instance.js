2c30aee9093877af61f17ebbb2f2040a2195afb6
Eugene Badato <badatoeugenepaulm@gmail.com>
2021-09-03 16:44:08 +0800

    feat: added diff servers in env

--- src/axios/axios-instance.js
+++ src/axios/axios-instance.js
@@ -2,9 +2,6 @@ import axios from "axios";
 import store from "@/store";
 
 const instance = axios.create({
-	//baseURL: 'https://payroll-ent-cloud.herokuapp.com/api/', // heroku
-	//baseURL: 'http://159.138.137.115:5000/', // server
-	// baseURL: 'http://pecbe-env.eba-jrab52bp.us-east-2.elasticbeanstalk.com/api/',
 	baseURL: process.env.VUE_APP_BASE_URL_SERVER,
 	headers: {
 		Authorization: `Bearer ${store.getters.getToken}`,
@@ -12,9 +9,3 @@ const instance = axios.create({
 });
 
 export default instance;
-
-// const instance = axios.create({
-//   baseURL: 'https://some-domain.com/api/',
-//   timeout: 1000,
-//   headers: {'X-Custom-Header': 'foobar'}
-// });
